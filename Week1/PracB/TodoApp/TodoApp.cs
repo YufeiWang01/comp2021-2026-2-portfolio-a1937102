ac6d6b9fde78a9dbf17f42f4cff0852755b59e75
YufeiWang01 <yufeiwang_2024@outlook.com>
2026-08-09 22:19:31 +0800

    PracB-Task2 code and screenshot

--- Week1/PracB/TodoApp/TodoApp.cs
+++ Week1/PracB/TodoApp/TodoApp.cs
@@ -6,17 +6,19 @@ class TodoApp
     {
         List<string> tasks = new List<string>();
 
+        Dictionary<string, List<int>> tags = new Dictionary<string, List<int>>();
+
         while (true)
 
         {
-            Console.Write("Enter command (add, show, remove, clear): ");
+            Console.Write("Enter command (add, show, remove, clear, tag, get-tagged): ");
             string command = Console.ReadLine() ?? "";
 
             switch (command)
 
             {
                 case "add":
-                
+
                     Console.Write("Enter task: ");
                     string task = Console.ReadLine() ?? "";
 
@@ -59,9 +61,59 @@ class TodoApp
                     tasks.Clear();
                     break;
 
+                case "tag":
+                Console.Write("Enter index: ");
+                int tagIndex;
+
+                if (!int.TryParse(Console.ReadLine(), out tagIndex) ||
+                    tagIndex < 0 || tagIndex >= tasks.Count)
+                {
+                    Console.WriteLine("Invalid index.");
+                    break;
+                }
+
+                Console.Write("Enter tag: ");
+                string tagName = Console.ReadLine() ?? "";
+
+                if (!tags.ContainsKey(tagName))
+                {
+                    tags[tagName] = new List<int>();
+                }
+
+                if (tags[tagName].Contains(tagIndex))
+                {
+                    Console.WriteLine("Tag already added.");
+                    break;
+                }
+
+                tags[tagName].Add(tagIndex);
+                Console.WriteLine("Tag added.");
+                break;
+
+                case "get-tagged":
+                Console.Write("Enter tag: ");
+                string searchTag = Console.ReadLine() ?? "";
+
+                if (!tags.ContainsKey(searchTag))
+                {
+                    Console.WriteLine("Tag not found.");
+                    break;
+                }
+
+                foreach (int taskIndex in tags[searchTag])
+                {
+                    Console.WriteLine($"{taskIndex}: {tasks[taskIndex]}");
+                }
+                break;
+
+
+
                 default:
                     Console.WriteLine("Invalid command.");
                     break;
+
+
+                
             }
         }
     }
