ae5588afd2e618ade32412a6c1a74d54865a0614
YufeiWang01 <yufeiwang_2024@outlook.com>
2026-08-09 22:28:53 +0800

    PracB-Task3 code and screenshot

---
+++ Week1/PracB/TodoApp.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿
+public class UnitTest1
+{
+    [Fact]
+    public void FullName_ReturnsExpectedFormat()
+    {
+        Person person = new Person("Yufei", "Wang", 20);
+
+        string result = person.FullName();
+
+        Assert.Equal("Wang, Yufei", result);
+    }
+
+    [Fact]
+    public void IsAdult_ReturnsTrue_WhenAge18OrMore()
+    {
+        Person person = new Person("Yufei", "Wang", 20);
+
+        bool result = person.IsAdult();
+
+        Assert.True(result);
+    }
+}
