5e0bba364dcd425b61368d5d102aa47b671a01c1
Yufei Wang <yufeiwang_2024@outlook.com>
2026-08-11 13:50:25 +0930

    feat: complete Week 2 Task 1

---
+++ Week2/PracA/Payroll/Program.cs
@@ -0,0 +1,7 @@
+﻿Payroll payroll = new Payroll(40, 20.00m, 0.2m);
+
+Console.WriteLine($"Net pay: {payroll.CalculateNetpay():F2}");
+
+payroll.ChangeTaxRate(0.25m);
+
+Console.WriteLine($"New Net pay: ${payroll.CalculateNetpay():F2}");
